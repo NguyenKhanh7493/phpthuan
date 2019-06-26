764ed3e999648b2ea02d5ff1d995279594f5a48a
NguyenKhanh7493 <nguyenkhanh7493@gmail.com>
2019-06-26 21:56:34 +0700

    ok phan trang

--- admin/module/user/list.php
+++ admin/module/user/list.php
@@ -3,8 +3,22 @@
     include (__DIR__.'/../../config/define.php');
     include (__DIR__.'/../../config/database.php');
     $title = "Danh sách sản phẩm";
-    $sql = "SELECT * FROM users ";
+    $sql = "SELECT count(id) as total FROM users";
     $result = mysqli_query($db,$sql);
+    $row = mysqli_fetch_assoc($result);
+    //
+    $total_records = $row['total'];
+    $current_page = isset($_GET['page'])?$_GET['page']:1;
+    $limit= 2;
+    $total_page = ceil($total_records/$limit);
+    if ($current_page > $total_page){
+        $current_page = $total_page;
+    }elseif ($current_page < 1){
+        $current_page = 1;
+    }
+    $start = ($current_page - 1)*$limit;
+    $sql = "SELECT * FROM users LIMIT $start,$limit";
+    $target = mysqli_query($db,$sql);
 ?>
 <?php include (__DIR__.'/../../layout/head.php');?>
     <div class="row bg-title">
@@ -48,7 +62,7 @@
                     </tr>
                     </thead>
                     <tbody>
-                    <?php while ($item = mysqli_fetch_assoc($result)){?>
+                    <?php while ($item = mysqli_fetch_assoc($target)){?>
                         <tr id="<?=$item['id']?>">
                             <td><?php echo $item['id']?></td>
                             <td><?php echo $item['fullname']?></td>
@@ -69,4 +83,43 @@
         </div>
     </div>
 </div>
+<!--test-->
+    <nav aria-label="...">
+        <ul class="pagination">
+            <?php if ($current_page > 1 && $total_page > 1){
+                echo '<li class="page-item disabled"><a href="/admin/module/user/list.php?page='.($current_page - 1).'" class="page-link" >Lùi</a></li>';
+            }?>
+            <?php
+            for ($i=1;$i<=$total_page;$i++){
+                if ($i == $current_page){
+                    echo '<li class="page-item active"><span class="page-link">'.$i.'<span class="sr-only">(current)</span></span></li>';
+                }else{
+                    echo '<li class="page-item"><a class="page-link" href="/admin/module/user/list.php?page='.$i.'">'.$i.'</a></li>';
+                }
+            }
+            ?>
+            <?php if ($current_page < $total_page && $total_page > 1){
+                echo '<li class="page-item"><a class="page-link" href="/admin/module/user/list.php?page='.($current_page + 1).'">Tới</a></li>';
+            }?>
+        </ul>
+    </nav>
+<!--end-->
+    <nav aria-label="...">
+        <ul class="pagination">
+            <li class="page-item disabled">
+                <span class="page-link">Previous</span>
+            </li>
+            <li class="page-item"><a class="page-link" href="#">1</a></li>
+            <li class="page-item active">
+      <span class="page-link">
+        2
+        <span class="sr-only">(current)</span>
+      </span>
+            </li>
+            <li class="page-item"><a class="page-link" href="#">3</a></li>
+            <li class="page-item">
+                <a class="page-link" href="#">Next</a>
+            </li>
+        </ul>
+    </nav>
 <?php include (__DIR__.'/../../layout/footer.php');?>
